6761801f75ee3a99a611e3d56a360d15063300c8
Prashant Palkar <ppalkar.iitb@gmail.com>
2021-12-22 06:31:54 +0530

    Some simple bash scripts (for doing runs in parallel).

---
+++ leonRuns.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+## args in sequence: insName timeHorizon maxIter precision alpha timeStep
+time python3 main.py leon 60 1000 0.1 0.5 0.5        &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.1_0.500_0.5
+time python3 main.py leon 60 1000 0.1 0.5 0.25       &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.1_0.500_0.25
+time python3 main.py leon 60 1000 0.1 0.5 0.125      &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.1_0.500_0.125
+time python3 main.py leon 60 1000 0.1 0.5 0.03125    &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.1_0.500_0.03125
+#                                                                                       
+time python3 main.py leon 60 1000 0.1 0.25 0.5       &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.1_0.250_0.5
+time python3 main.py leon 60 1000 0.1 0.25 0.25      &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.1_0.250_0.25
+time python3 main.py leon 60 1000 0.1 0.25 0.125     &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.1_0.250_0.125
+time python3 main.py leon 60 1000 0.1 0.25 0.03125   &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.1_0.250_0.03125
+#                                                                                    
+time python3 main.py leon 60 1000 0.1 0.125 0.5      &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.1_0.125_0.5
+time python3 main.py leon 60 1000 0.1 0.125 0.25     &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.1_0.125_0.25
+time python3 main.py leon 60 1000 0.1 0.125 0.125    &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.1_0.125_0.125
+time python3 main.py leon 60 1000 0.1 0.125 0.03125  &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.1_0.125_0.03125
+####
+time python3 main.py leon 60 1000 0.05 0.5 0.5       &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.05_0.500_0.5
+time python3 main.py leon 60 1000 0.05 0.5 0.25      &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.05_0.500_0.25
+time python3 main.py leon 60 1000 0.05 0.5 0.125     &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.05_0.500_0.125
+time python3 main.py leon 60 1000 0.05 0.5 0.03125   &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.05_0.500_0.03125
+#                                                                                      
+time python3 main.py leon 60 1000 0.05 0.25 0.5      &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.05_0.250_0.5
+time python3 main.py leon 60 1000 0.05 0.25 0.25     &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.05_0.250_0.25
+time python3 main.py leon 60 1000 0.05 0.25 0.125    &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.05_0.250_0.125
+time python3 main.py leon 60 1000 0.05 0.25 0.03125  &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.05_0.250_0.03125
+#                                                                                      
+time python3 main.py leon 60 1000 0.05 0.125 0.5     &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.05_0.125_0.5
+time python3 main.py leon 60 1000 0.05 0.125 0.25    &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.05_0.125_0.25
+time python3 main.py leon 60 1000 0.05 0.125 0.125   &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.05_0.125_0.125
+time python3 main.py leon 60 1000 0.05 0.125 0.03125 &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.05_0.125_0.03125
+####
+time python3 main.py leon 60 1000 0.01 0.5 0.5       &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.01_0.500_0.5
+time python3 main.py leon 60 1000 0.01 0.5 0.25      &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.01_0.500_0.25
+time python3 main.py leon 60 1000 0.01 0.5 0.125     &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.01_0.500_0.125
+time python3 main.py leon 60 1000 0.01 0.5 0.03125   &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.01_0.500_0.03125
+#                                                                                      
+time python3 main.py leon 60 1000 0.01 0.25 0.5      &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.01_0.250_0.5
+time python3 main.py leon 60 1000 0.01 0.25 0.25     &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.01_0.250_0.25
+time python3 main.py leon 60 1000 0.01 0.25 0.125    &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.01_0.250_0.125
+time python3 main.py leon 60 1000 0.01 0.25 0.03125  &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.01_0.250_0.03125
+#                                                                                      
+time python3 main.py leon 60 1000 0.01 0.125 0.5     &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.01_0.125_0.5
+time python3 main.py leon 60 1000 0.01 0.125 0.25    &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.01_0.125_0.25
+time python3 main.py leon 60 1000 0.01 0.125 0.125   &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.01_0.125_0.125
+time python3 main.py leon 60 1000 0.01 0.125 0.03125 &>   /home/prashantpalkarext/flow-algorithms/results/leon_60_1000_0.01_0.125_0.03125
